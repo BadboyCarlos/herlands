8430951521a5a9961af049cf17d8a1034569edcc
lvxin <tztc1214@foxmail.com>
2018-08-14 23:54:16 +0800

    第一次尝试

---
+++ src/main/java/com/example/herlands/HerlandsApplication.java
@@ -0,0 +1,12 @@
+package com.example.herlands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerlandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerlandsApplication.class, args);
+    }
+}
